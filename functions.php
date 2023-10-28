e03f4e77f243bb1607559344e5eccf215ee270a1
francisco-wps <francisco@webprogressivesolutions.com>
2023-10-28 12:40:49 +0100

    policy cookies added

--- functions.php
+++ functions.php
@@ -33,5 +33,19 @@ include( get_template_directory() . '/templates/shortcodes/work_projects_cards.p
 include( get_template_directory() . '/templates/shortcodes/post_projects.php' );
 
 
+/* Describe what the code snippet does so you can remember later on */
+add_action('wp_head', 'your_function_name');
+function your_function_name(){
+?>
+    <script src="https://cdn.websitepolicies.io/lib/cconsent/cconsent.min.js" defer></script><script>window.addEventListener("load",function(){window.wpcb.init({"border":"thin","corners":"small","colors":{"popup":{"background":"#ffe4e1","text":"#000000","border":"#c25e5e"},"button":{"background":"#c25e5e","text":"#ffffff"}},"position":"bottom","content":{"href":"https://frank-moreno.com/cookie-policy/"}})});</script>
+<?php
+};
 
 
+/* Describe what the code snippet does so you can remember later on */
+add_action('wp_footer', 'your_function_name_2');
+function your_function_name_2(){
+?>
+<!-- PASTE FOOTER CODE HERE -->
+<?php
+};
